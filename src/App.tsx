d78ca956c59d6aa884750eec36230a49479d666a
dayana-sog <dayanasog23@gmail.com>
2020-08-11 12:48:06 +0100

    resolução desafio 07

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <Router>
+      <Routes />
+      <ToastContainer autoClose={3000} />
+    </Router>
+  </>
+);
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F0F2F5 ;
+    -webkit-font-smoothing: antialiased
+  }
+
+  body, input, button {
+    font: 16px "Poppins", sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  .toast {
+    background-color: #ffbb00;
+    font-size: 14px;
+    color: #363F5F;
+    font-weight: bold;
+  }
+`;
